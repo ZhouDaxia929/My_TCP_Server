add14cf16443cadaf7d4d925d8577e25f6dfdb83
ZhouDaxia <zhoudongxu929@outlook.com>
2019-11-27 01:08:34 +0800

    Add Some functions to operator socket

---
+++ net/SocketOperation.h
@@ -0,0 +1,50 @@
+#ifndef MTS_SOCKET_OPERATION
+#define MTS_SOCKET_OPERATION
+
+#include <sys/types.h>
+#include <string>
+#include <netinet/in.h>
+#include <linux/tcp.h>
+
+namespace My_TCP_Server{
+
+namespace net{
+
+class SocketOperation{
+public:
+    static const int32_t Ipv4AddrAny;
+    static int createNonblockingSocket();
+    static int bind(int sockfd, const struct sockaddr_in *addr); 
+    static int listen(int sockfd);
+    static int connect(int sockfd, const struct sockaddr *addr);
+    static int accept(int sockfd, const struct sockaddr_in *addr);
+    static ssize_t read(int sockfd, void *buf, size_t count);
+    static ssize_t readv(int sockfd, const struct iovec *iov, int iovcnt);
+    static ssize_t write(int sockfd, const void *buf, size_t count);
+    static void close(int sockfd);
+    static void getAddrAnyIpv4(struct sockaddr_in &addrIn, uint16_t port);
+    /*
+     * 分离IP地址字符串中的端口号
+     */
+    static bool toAddrIpv4(const std::string &addrIp, struct sockaddr_in &addrIn);
+    /*
+     * 分离IP地址字符串中的ip的点分十进制
+     */
+    static bool toAddrIpv4(const std::string &addr, uint16_t port, struct sockaddr_in &addrIn);
+    static bool toAddrIpv4(uint16_t port, struct sockaddr_in &addrIn);
+    static std::string ipToString(struct sockaddr_in addr);
+    static std::string toString(struct sockaddr_in addr);
+
+    template<typename T>
+    static bool stringToInt(const std::string &str, T &num);
+
+    static int shutdownWrite(int sockfd);
+    static void setTcpNoDelay(int fd, bool isEnable);
+};
+
+
+}
+}
+
+
+#endif
